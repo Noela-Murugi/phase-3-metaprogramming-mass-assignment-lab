a59e977f6c9bf7d553904cb9a254a8c2de547b18
Noela-Murugi <murugin254@gmail.com>
2022-10-24 14:54:43 +0300

    completed the assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,15 @@
 class Person
   # your code here
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=",value)
+    end
+  end
 end
+
+# attributes = { name: "Noela", hair_color: "Black" }
+
+# noela = Person.new(attributes)
+# noela.name
+# noela.hair_color
